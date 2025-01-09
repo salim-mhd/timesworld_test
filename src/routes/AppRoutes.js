eeb6edc4734f5268b86fe5eed326aa271ef4dacf
muahmmed salim <salimmhd1280@gmail.com>
2025-01-10 05:12:27 +0530

    bug fixed

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,27 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../components/authentication/login/Login";
 import Home from "../components/home/Home";
 
 const AppRoutes = () => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated");
-
   return (
     <Router>
       <Routes>
         {/* Login Page */}
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
         {/* Home Page - Protected Route */}
-        <Route
-          path="/home"
-          element={isAuthenticated ? <Home /> : <Navigate to="/" replace />}
-        />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
